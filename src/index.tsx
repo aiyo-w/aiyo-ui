e9e486d4320a8a4447342635c533df132046ef2a
aiyo-w <aiyo_w@outlook.com>
2022-05-08 18:59:06 +0800

    add use button component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { AiyoButton } from './aiyo-button';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AiyoButton className="test" iconClass='icon-smail'>apao</AiyoButton>
   </React.StrictMode>
 );
 
