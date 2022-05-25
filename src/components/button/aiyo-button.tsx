8c26d82a25d26bda457b77770685e6361684f044
aiyo-w <aiyo_w@outlook.com>
2022-05-25 22:05:39 +0800

    modufy button style

--- src/components/button/aiyo-button.tsx
+++ src/components/button/aiyo-button.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, PropsWithChildren } from "react";
+import React, { CSSProperties, MouseEventHandler, PropsWithChildren } from "react";
 import "./aiyo-button.css"
 
 interface IProps {
@@ -8,12 +8,13 @@ interface IProps {
     disabled?: boolean;
     plain?: boolean;
     loading?: boolean;
+    style?: CSSProperties;
     onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 export const AiyoButton = (props: PropsWithChildren<IProps>) => {
     return (<button className={`ai-btn ai-btn-${props.shape ? props.shape : "rectangle"} 
     ai-btn-${props.plain ? "plain" : "default"} 
-    ${props.className ? props.className : ""}`} disabled={props.disabled} onClick={props.onClick}>
+    ${props.className ? props.className : ""}`} style={props.style} disabled={props.disabled} onClick={props.onClick}>
         {!props.loading && props.iconClass && <span className={props.iconClass}></span>}
         {props.loading && <span className={`ai-btn-loading-${props.plain ? "plain" : "default"}`}></span>}
         {props.iconClass && props.children && !(props.shape === "circle" || props.shape === "square") && <div className="icon-spacing-text"></div>}
