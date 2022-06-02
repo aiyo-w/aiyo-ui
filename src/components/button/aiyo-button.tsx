9646ed91e2a7da819fb962be335cdfca0d002f57
aiyo-w <aiyo_w@outlook.com>
2022-06-02 15:00:11 +0800

    update 优化

--- src/components/button/aiyo-button.tsx
+++ src/components/button/aiyo-button.tsx
@@ -4,20 +4,35 @@ import "./aiyo-button.css"
 interface IProps {
     iconClass?: string;
     className?: string;
-    shape?: string;
+    shape?: 'rounded' | 'circle' | 'rectangle' | 'square';
     disabled?: boolean;
     plain?: boolean;
+    text?: boolean;
     loading?: boolean;
     style?: CSSProperties;
     onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 export const AiyoButton = (props: PropsWithChildren<IProps>) => {
-    return (<button className={`ai-btn ai-btn-${props.shape ? props.shape : "rectangle"} 
-    ai-btn-${props.plain ? "plain" : "default"} 
-    ${props.className ? props.className : ""}`} style={props.style} disabled={props.disabled} onClick={props.onClick}>
-        {!props.loading && props.iconClass && <span className={props.iconClass}></span>}
-        {props.loading && <span className={`ai-btn-loading-${props.plain ? "plain" : "default"}`}></span>}
-        {props.iconClass && props.children && !(props.shape === "circle" || props.shape === "square") && <div className="icon-spacing-text"></div>}
-        {props.shape === "circle" || props.shape === "square" ? "" : <span>{props.children}</span>}
-    </button>);
+    return (
+        <div className="aiyo-btn">
+            <button className={`ai-btn ${props.shape} ${props.plain && 'plain'} ${props.text && 'text'} ${props.className ? props.className : ""}`}
+                style={props.style}
+                disabled={props.disabled}
+                onClick={props.onClick}
+            >
+                {!props.loading && props.iconClass && <span className={props.iconClass}></span>}
+                {props.loading && <span className={`ai-btn-loading-${props.plain ? "plain" : "default"}`}></span>}
+                {props.children && !(props.shape === "circle" || props.shape === "square") && <div className="icon-spacing-text"></div>}
+                {props.shape === "circle" || props.shape === "square" ? "" : <span>{props.children}</span>}
+            </button>
+        </div>
+    );
+}
+
+AiyoButton.defaultProps = {
+    shape: 'rectangle',
+    disabled: false,
+    plain: false,
+    loading: false,
+    text: false,
 }
